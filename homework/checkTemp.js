bdf44ee7c215d65a3fefe40288034573a24689bf
anzelFj <92165078+anzelFj@users.noreply.github.com>
2021-10-14 17:09:09 +0300

    Update checkTemp.js

--- homework/checkTemp.js
+++ homework/checkTemp.js
@@ -2,33 +2,16 @@ function checkTemp (temp) {
     if (temp >= 0 && temp <= 20)
 {
     console.log ("Liiga külm");
-}
-if (temp >= 21 && temp <= 40)
-{
-    console.log("Temp on paras")
-}
-if (temp >= 41 && temp <= 60)
-{
-    console.log("Liiga kuum")
-}
-}
-
-
-
-function checkTemp (temp) {
-    if (temp >= 0 && temp <= 20)
-{
-    console.log ("Liiga külm");
-        return "-1"
+        return -1
 }
 if (temp >= 21 && temp <= 40)
 {
     console.log("Temp on paras");
-        return "0"
+        return 0
 }
 if (temp >= 41 && temp <= 60)
 {
     console.log("Liiga kuum");
-        return "+1"
+        return +1
 }
 }
